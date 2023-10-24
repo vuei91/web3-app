7878034c50869f0d497ea673c856356f40526579
rosette777 <wkwk2805u@zcube.co.kr>
2023-10-24 21:35:27 +0900

    메타마스크 연결화면

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,24 @@
+"use client";
+import Profile from "@/components/Profile";
 import React from "react";
+import { WagmiConfig, createConfig, configureChains, mainnet } from "wagmi";
+import { InjectedConnector } from "wagmi/connectors/injected";
+import { publicProvider } from "wagmi/providers/public";
+
+const { chains, publicClient } = configureChains([mainnet], [publicProvider()]);
+
+const config = createConfig({
+  publicClient,
+  autoConnect: true,
+  connectors: [new InjectedConnector({ chains })],
+});
 
 const Home = () => {
-  return <div>Hello world</div>;
+  return (
+    <WagmiConfig config={config}>
+      <Profile />
+    </WagmiConfig>
+  );
 };
 
 export default Home;
---
+++ components/Profile.js
@@ -0,0 +1,35 @@
+"use client";
+import { useEffect, useState } from "react";
+import { useConnect } from "wagmi";
+
+const Profile = () => {
+  const [mount, setMount] = useState(false);
+  const { connect, connectors, error, isLoading, pendingConnector } =
+    useConnect();
+  useEffect(() => {
+    setMount(true);
+  }, []);
+  return (
+    mount && (
+      <div>
+        {connectors.map((connector) => (
+          <button
+            disabled={!connector.ready}
+            key={connector.id}
+            onClick={() => connect({ connector })}
+          >
+            {connector.name}
+            {!connector.ready && " (unsupported)"}
+            {isLoading &&
+              connector.id === pendingConnector?.id &&
+              " (connecting)"}
+          </button>
+        ))}
+
+        {error && <div>{error.message}</div>}
+      </div>
+    )
+  );
+};
+
+export default Profile;
