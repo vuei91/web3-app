af1a372c6a10ff1e945a889c5bba2e219843a39d
rosette777 <wkwk2805u@zcube.co.kr>
2023-10-24 21:59:26 +0900

    다른 지갑도 적용

--- app/providers.jsx
+++ app/providers.jsx
@@ -1,15 +1,30 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import { WagmiConfig, createConfig, configureChains, sepolia } from "wagmi";
-import { InjectedConnector } from "wagmi/connectors/injected";
 import { publicProvider } from "wagmi/providers/public";
+
+// connectors
+import { CoinbaseWalletConnector } from "wagmi/connectors/coinbaseWallet";
+import { InjectedConnector } from "wagmi/connectors/injected";
+import { MetaMaskConnector } from "wagmi/connectors/metaMask";
+import { WalletConnectConnector } from "wagmi/connectors/walletConnect";
 
 const { chains, publicClient } = configureChains([sepolia], [publicProvider()]);
 
 const config = createConfig({
   publicClient,
   autoConnect: true,
-  connectors: [new InjectedConnector({ chains })],
+  connectors: [
+    new MetaMaskConnector({ chains }),
+    new CoinbaseWalletConnector({ chains }),
+    new WalletConnectConnector({
+      chains,
+      options: {
+        projectId: "e72f727f24a0188d8b820164bfc49747", // wallet connector에 가입 후 프로젝트를 생성하여 그 ID를 넣어야함
+      },
+    }),
+    new InjectedConnector({ chains }),
+  ],
 });
 
 const Providers = ({ children }) => {
