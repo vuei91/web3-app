28a52f1af93d5ede841515c26271273e41a23bd4
rosette777 <wkwk2805u@zcube.co.kr>
2023-10-25 19:45:44 +0900

    information 변경

--- components/Information.js
+++ components/Information.js
@@ -9,26 +9,42 @@ import {
 } from "wagmi";
 
 const Informaion = () => {
-  const { address, isConnecting, isDisconnected } = useAccount(); // 현재 연결된 지갑의 정보를 가져옴
+  // 현재 연결된 지갑의 정보를 가져옴
+  const { address, isConnecting, isDisconnected } = useAccount();
   const { data: balance } = useBalance({ address });
   const { data: blockNumber } = useBlockNumber();
   const { chain, chains } = useNetwork();
-  const { data: token } = useToken({ address: "" });
+  const { data: token } = useToken({
+    address: "0xFd59f3889524DbeEEB07233a9eDe612fFAE78cE9",
+  });
   if (isConnecting) return <div>Connecting…</div>;
   if (isDisconnected) return <div>Disconnected</div>;
   return (
     <>
-      <div>address: {address}</div>
       <div>
-        balance : {balance?.formatted} symbol: {balance?.symbol}
+        <strong>address:</strong> {address}
       </div>
-      <div>blocknumber: {parseInt(blockNumber)}</div>
-      <div>chain name: {chain.name}</div>
+      <div>
+        <strong>balance :</strong> {balance?.formatted} <strong>symbol:</strong>{" "}
+        {balance?.symbol}
+      </div>
+      <div>
+        <strong>blocknumber:</strong> {parseInt(blockNumber)}
+      </div>
+      <div>
+        <strong>chain name:</strong> {chain.name}
+      </div>
       {chains && (
-        <div>Available chains: {chains.map((chain) => chain.name)}</div>
+        <div>
+          <strong>Available chains:</strong> {chains.map((chain) => chain.name)}
+        </div>
       )}
       <div>
-        token name: {token?.name} {token?.symbol}
+        <strong>token name:</strong> {token?.name}{" "}
+        <strong>token symbol:</strong> {token?.symbol}
+      </div>
+      <div>
+        <strong>token totalSupply:</strong> {token?.totalSupply?.formatted}
       </div>
     </>
   );
